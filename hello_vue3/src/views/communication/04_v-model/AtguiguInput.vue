ff3643f45816060234764c1202816a19bab21848
lcruan <1416802331@qq.com>
2024-07-23 22:03:04 +0800

    v-model细节

--- hello_vue3/src/views/communication/04_v-model/AtguiguInput.vue
+++ hello_vue3/src/views/communication/04_v-model/AtguiguInput.vue
@@ -1,14 +1,27 @@
 <template>
-  <input
+  <!-- <input
     type="text"
     :value="modelValue"
     @input="emit('update:modelValue', (<HTMLInputElement>$event.target).value)"
+  /> -->
+  <input
+    type="text"
+    :value="ming"
+    @input="emit('update:ming', (<HTMLInputElement>$event.target).value)"
+  />
+  <br/>
+  <input
+    type="text"
+    :value="mima"
+    @input="emit('update:mima', (<HTMLInputElement>$event.target).value)"
   />
 </template>
 
 <script setup lang="ts" name="AtguiguInput">
-defineProps(["modelValue"]);
-const emit = defineEmits(["update:modelValue"]);
+// defineProps(["modelValue"]);
+defineProps(["ming", "mima"]);
+// const emit = defineEmits(["update:modelValue"]);
+const emit = defineEmits(["update:ming", "update:mima"]);
 </script>
 <style scoped>
 input {
--- hello_vue3/src/views/communication/04_v-model/Father.vue
+++ hello_vue3/src/views/communication/04_v-model/Father.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="father">
     <h3>父组件</h3>
+    <h4>{{ username }}</h4>
+    <h4>{{ password }}</h4>
     <!-- v-model用在html标签上 -->
     <!-- <input type="text" v-model="username"/> -->
     <!-- 第5行顶层实现代码如下   v-model底层原理是动态value值配合input事件-->
     <!-- <input type="text" :value="username" @input="username = (<HTMLInputElement>$event.target).value"> -->
 
     <!-- v-model用在组件标签上 -->
-    <AtguiguInput v-model="username"/>
+    <!-- <AtguiguInput v-model="username"/> -->
     <!-- 本质写法   update:modelValue就是一个事件名-->
     <!-- <AtguiguInput
       :modelValue="username"
       @update:modelValue="username = $event"
     /> -->
+
+    <!-- 修改modelValue -->
+    <AtguiguInput v-model:ming="username" v-model:mima="password"/>
   </div>
 </template>
 
@@ -21,6 +26,7 @@ import { ref } from "vue";
 import AtguiguInput from "./AtguiguInput.vue";
 
 let username = ref("zhangsan");
+let password = ref('123456')
 </script>
 
 <style scoped></style>
